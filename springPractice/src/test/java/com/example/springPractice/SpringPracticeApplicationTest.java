ff1e36e0daa2b106559e8cca4cd22c14f64dc6fa
L-YS-123 <yoonsunglee123@gmail.com>
2021-12-07 00:25:12 +0900

    spring

---
+++ springPractice/src/test/java/com/example/springPractice/SpringPracticeApplicationTest.java
@@ -0,0 +1,14 @@
+package com.example.springPractice;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SpringPracticeApplicationTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
