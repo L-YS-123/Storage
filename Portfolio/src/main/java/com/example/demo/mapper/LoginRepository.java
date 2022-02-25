d4d2d3f7c0588c1e44d9e1c97cdc7bbe1b238b21
lys1111 <yoonsunglee12345@gmail.com>
2022-02-25 22:39:28 +0900

    commit

---
+++ Portfolio/src/main/java/com/example/demo/mapper/LoginRepository.java
@@ -0,0 +1,11 @@
+package com.example.demo.mapper;
+
+
+//repository에는 DB에 접근하는 코드가 모여있다
+
+@mapper
+public interface LoginRepository {
+
+	
+	
+}
--- Portfolio/src/main/java/com/example/demo/service/LoginService.java
+++ Portfolio/src/main/java/com/example/demo/service/LoginService.java
@@ -2,7 +2,7 @@ package com.example.demo.service;
 
 import org.springframework.stereotype.Service;
 
-import com.example.demo.repository.LoginRepository.doLogin;
+import com.example.demo.mapper.LoginRepository.doLogin;
 
 //service에는 비즈니스 로직이 집중되어 있다.
 @Service
